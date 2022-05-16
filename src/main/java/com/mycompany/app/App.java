ddee0802aa0a3ed5be4221b66ea4d52a0e917e60
Quang Nguyễn <nguyenquanguet@gmail.com>
2022-05-16 13:14:33 +0700

    Demo Project

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,15 @@
+package com.mycompany.app;
+
+import com.mycompany.app.myplugin.Maven_Demo;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int a = Maven_Demo.tinhtong(4,2);
+        System.out.println( "Hello World!" + a );
+    }
+}
---
+++ src/main/java/com/mycompany/app/myplugin/Maven_Demo.java
@@ -0,0 +1,8 @@
+package com.mycompany.app.myplugin;
+
+public class Maven_Demo {
+
+    public static int tinhtong(int a, int b){
+        return a+b;
+    }
+}
